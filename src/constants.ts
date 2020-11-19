fd187217f6ad865524198cf664df67b0db2ff2df
Max Koon <theakman96@gmail.com>
2020-11-19 12:17:44 -0500

    feat: added more dev deps

--- src/constants.ts
+++ src/constants.ts
@@ -4,6 +4,11 @@ export const PACKAGES = {
         dev: {
             typescript: '4.0.3',
             '@types/node': '14.14.0',
+            '@typescript-eslint/eslint-plugin': '4.7.0',
+            '@typescript-eslint/parser': '4.7.0',
+            eslint: '7.13.0',
+            'eslint-plugin-jsdoc': '30.7.7',
+            prettier: '^2.1.2',
         },
     },
 };
